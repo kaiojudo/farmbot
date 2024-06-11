83e855650956776bc967adf2880f6d43c94beb72
Hiếu Vũ <85061650+kaiojudo@users.noreply.github.com>
2024-06-11 10:38:09 +0700

    .

--- src/components/FarmField.jsx
+++ src/components/FarmField.jsx
@@ -6,7 +6,7 @@ export default function FarmField(props) {
         <div>
             <p>Coin: ${farm?.toFixed(4)}</p>
             <p>{user.farmSpeed * 60} xLOTP/hour</p>
-            <p>Next Claim: {(user.lastClaimTime.getTime() + 6 * 60 * 60 * 1000).toLocaleString()}</p>
+            <p>Next Claim: {user.lastClaimTime}</p>
             <button onClick={claimCoin}>Claim</button>
             <button onClick={showLevelUp}>Level Up</button>
             <p>Invite Link: https://t.me/tele_farming_bot?start={user.inviteRef}</p>
