57a4b9a7224839e6bb15a2f70b4e6d3042997ed3
Hiếu Vũ <85061650+kaiojudo@users.noreply.github.com>
2024-06-15 09:36:04 +0700

    .

---
+++ src/components/WalletInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WalletInfo = ({ walletAddress }) => {
+  return <p>Connected Wallet: {walletAddress}</p>;
+};
+
+export default WalletInfo;
---
+++ src/tonConnectConfig.jsx
@@ -0,0 +1,7 @@
+import { TonConnect } from '@tonconnect/sdk';
+
+const connector = new TonConnect({
+    manifestUrl: 'https://pokegram.games/tonconnect-manifest.json' // Thay đổi URL này thành URL của tệp manifest của bạn
+});
+
+export default connector;
