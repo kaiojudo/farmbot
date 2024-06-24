4a5b2141e44f0fc6c887421121659e50930f2b42
Hiếu Vũ <85061650+kaiojudo@users.noreply.github.com>
2024-06-24 14:51:00 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -503,11 +503,11 @@ function App() {
     try {
       const userId = user.userId;
       let offlineCoin = 0;
-      if (user.totalOfflineTime < 21600 ) {
+      if (user.totalOfflineTime < 21600) {
         offlineCoin = user?.farmSpeed * rankBuff / 60 * user.totalOfflineTime * 1.6;
       }
       else {
-        offlineCoin = user?.farmSpeed * rankBuff / 60 * 21600  * 1.6;
+        offlineCoin = user?.farmSpeed * rankBuff / 60 * 21600 * 1.6;
       }
       const response = await axios.post(`https://pokegram.games/user/${userId}/claimoffline`, { offlineCoin });
       updateData();
@@ -622,7 +622,7 @@ function App() {
         Close WebApp
       </button>
       <CopyText />
-      {(user?.totalOfflineTime + offlineTime > 10 && showOffline) &&
+      {(user?.totalOfflineTime + offlineTime > 600 && showOffline) &&
         <Offline
           user={user}
           rankBuff={rankBuff}
