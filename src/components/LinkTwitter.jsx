fb57e1201c7b419d6a504ea9c39ec886deaab70d
Hiếu Vũ <85061650+kaiojudo@users.noreply.github.com>
2024-06-24 15:32:48 +0700

    Update LinkTwitter.jsx

--- src/components/LinkTwitter.jsx
+++ src/components/LinkTwitter.jsx
@@ -7,7 +7,7 @@ const LinkTwitter = () => {
 
     const handleLinkTwitter = async () => {
         try {
-            const response = await axios.get('http://pokegram.games/auth/twitter'); // Thay đổi thành endpoint backend của bạn
+            const response = await axios.get('https://pokegram.games/auth/twitter'); // Thay đổi thành endpoint backend của bạn
             const { redirectURL } = response.data;
 
             window.location.href = redirectURL; // Chuyển hướng người dùng đến Twitter OAuth
